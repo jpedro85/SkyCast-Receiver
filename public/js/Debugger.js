14308176aa92d97fa1c48812aff32209b4997cf0
jpedro <joao.p.abreu@hotmail.com>
2024-02-23 17:02:16 +0000

    feat: converted Debugger to classes

--- public/js/Debugger.js
+++ public/js/Debugger.js
@@ -1,34 +1,58 @@
-/* eslint-disable no-undef */
+class DebuggerConsole {
 
-const DebugMessagesEnum = {
-    INFO: "info",
-    DEBUG: "debug",
-    ERROR: "error",
-    WARN: "warn",
-};
+    constructor() {
+        if (DebuggerConsole.instance instanceof DebuggerConsole) {
+            return DebuggerConsole.instance;
+        }
 
-function enableDebugOverlay(debugLogs = false) {
-    debuggerConsole.setEnabled(true);
-    debuggerConsole.showDebugLogs(debugLogs)
-}
+        // Initialize your class instance here
+        this.DebugMessagesEnum = {
+            INFO: "info",
+            DEBUG: "debug",
+            ERROR: "error",
+            WARN: "warn",
+        };
+        this.console = cast.debug.CastDebugLogger.getInstance();
+        this.tag = "DebuggerConsole.LOG";
+        // Keep a reference to the new instance
+        DebuggerConsole.instance = this;
 
-function sendLog(type, ...message) {
-    switch (type) {
-        case DebugMessagesEnum.DEBUG:
-            debuggerConsole.debug(this.logTag, message);
-            break;
-        case DebugMessagesEnum.INFO:
-            debuggerConsole.info(this.logTag, message);
-            break;
-        case DebugMessagesEnum.ERROR:
-            debuggerConsole.error(this.logTag, message);
-            break;
-        case DebugMessagesEnum.WARN:
-            debuggerConsole.warn(this.logTag, message);
-            break;
+        // Explicitly return the instance
+        return this;
+    }
 
-        default:
-            debuggerConsole.info(this.logTag, message);
-            break;
+    enableDebugOverlay(debugLogs = false) {
+        this.console.setEnabled(true);
+        this.console.showDebugLogs(debugLogs);
+        // Set verbosity level for Core events and custom tags.
+        this.console.loggerLevelByEvents = {
+            "cast.framework.events.category.CORE": cast.framework.LoggerLevel.INFO,
+            "cast.framework.events.EventType.MEDIA_STATUS": cast.framework.LoggerLevel.DEBUG,
+        };
+
+        this.console.loggerLevelByTags = {
+            logTag: cast.framework.LoggerLevel.DEBUG,
+        };
+    }
+
+    /**
+     * Function responsible for sending the Debugger logs.
+     * @param {string} type the type of message you want to send being the same as the values in DebugMessagesEnum
+     * @param {any[]} message The message that is going to be sent
+     */
+    sendLog(type, ...message) {
+        if (type == this.DebugMessagesEnum.DEBUG) {
+            this.console.debug(this, message);
+        } else if (type == this.DebugMessagesEnum.INFO) {
+            this.console.info(this.tag, message);
+        } else if (type == this.DebugMessagesEnum.ERROR) {
+            this.console.error(this.tag, message);
+        } else if (type == this.DebugMessagesEnum.WARN) {
+            this.console.warn(this.tag, message);
+        } else {
+            this.console.info(this.tag, message); // Default to info if type is not recognized
+        }
     }
 }
+
+export default DebuggerConsole;
--- public/js/Main.js
+++ public/js/Main.js
@@ -1,25 +1,62 @@
+import DebuggerConsole  from "./Debugger.js";
+
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 
-const debuggerConsole = cast.debug.CastDebugLogger.getInstance();
-const logTag = "DebuggerConsole.LOG";
-
-enableDebugOverlay();
-sendLog("warn", "Hello World");
+// Initializing the debugger
+const debuggerConsole = new DebuggerConsole();
+debuggerConsole.enableDebugOverlay(true);
 
 const NAMESPACE = "urn:x-cast:com.skycast.chromecast.communication";
-// let communicationConstants = {};
+let communicationConstants = {};
 
-// Set verbosity level for Core events and custom tags.
-// castDebugLogger.loggerLevelByEvents = {
-//   "cast.framework.events.category.CORE": cast.framework.LoggerLevel.INFO,
-//   "cast.framework.events.EventType.MEDIA_STATUS": cast.framework.LoggerLevel.DEBUG,
-// };
+// Custom Message Handler
+// context.addCustomMessageListener(NAMESPACE, onMessage);
 
-// castDebugLogger.loggerLevelByTags = {
-//   LOG_TAG: cast.framework.LoggerLevel.DEBUG,
-// };
+// playerManager.setMessageInterceptor(cast.framework.messages.MessageType.LOAD, async (request) => {
+//     sendLog("info", "Intercepting LOAD request");
 
-sendLog("warn", "Test");
+//     // Map contentId to entity
+//     if (request.media && request.media.entity) {
+//         request.media.contentId = request.media.entity;
+//     }
 
-context.start();
+//     try {
+//         // Fetch repository metadata
+//         const data = await makeRequest("GET", SAMPLE_URL);
+//         // Obtain resources by contentId from downloaded repository metadata.
+//         const item = data[request.media.contentId];
+//         sendLog("info", request);
+//         if (!item) {
+//             // Content could not be found in repository
+//             sendLog("error", "Content not found");
+//             throw new Error("Content not found");
+//         }
+
+//         // Adjusting request to make requested content playable
+//         request.media.contentType = TEST_STREAM_TYPE;
+
+//         // Configure player for content type
+//         request.media.contentUrl = TEST_STREAM_TYPE === StreamType.DASH ? item.stream.dash : item.stream.hls;
+//         if (TEST_STREAM_TYPE === StreamType.HLS) {
+//             request.media.hlsSegmentFormat = cast.framework.messages.HlsSegmentFormat.FMP4;
+//             request.media.hlsVideoSegmentFormat = cast.framework.messages.HlsVideoSegmentFormat.FMP4;
+//         }
+
+//         castDebugLogger.warn(LOG_TAG, "Playable URL:", request.media.contentUrl);
+
+//         // Add metadata
+//         request.media.metadata = new cast.framework.messages.GenericMediaMetadata();
+//         request.media.metadata.title = item.title;
+//         request.media.metadata.subtitle = item.author;
+
+//         return request;
+//     } catch (error) {
+//         castDebugLogger.error(LOG_TAG, "Error processing request:", error);
+//         throw error;
+//     }
+// });
+
+context.start({
+    disableIdleTimeout: true,
+});
